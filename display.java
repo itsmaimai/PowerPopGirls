7dd29b5560dace656902ca08ecb92969e9e385b1
Ignacio Yurag <nasheryurag7289@gmail.com>
2022-09-14 10:11:55 +0800

    ethyl added display.java

---
+++ display.java
@@ -0,0 +1,17 @@
+/*
+ */
+
+/**
+ *
+ * @author Student.admin
+ */
+public class display {
+     public static void main(String[] args){
+        addition add = new addition();
+        add.result();
+        subtraction sub = new subtraction();
+        sub.result();
+     
+
+    }
+}
