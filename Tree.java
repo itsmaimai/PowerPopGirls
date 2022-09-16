fa64f20d4b00eca829154fe1067ba1cf1c02683c
ethyl28 <112525491+ethyl28@users.noreply.github.com>
2022-09-16 22:11:05 +0800

    initial commit

---
+++ Tree.java
@@ -0,0 +1,16 @@
+public class Tree extends Plants{
+    void leaf(){
+        System.out.print("I am a leaf");
+    }
+    void branch(){
+        System.out.print("I am a branch");
+    }
+    void root(){
+        System.out.print("I am a root");
+    }
+    
+    public static void photosynthesis(){
+        System.out.println("Getting carbon dioxide from humans!");
+    }
+
+}
