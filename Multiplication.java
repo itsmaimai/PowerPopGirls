f67e39feed46b871e6fd7342e5c7d9845124ff47
bag2k <104628170+bag2k@users.noreply.github.com>
2022-09-16 20:14:27 +0800

    initial commit

--- Multiplication.java
+++ Multiplication.java
@@ -1,17 +1,4 @@
-import java.util.Scanner;
-public class Multiplication extends display{
-  void result(){
-        Scanner mul = new Scanner(System.in);
-        
-        System.out.println("MULTIPLICATION");
-        
-        System.out.print("Enter the first number: ");
-        int x = mul.nextInt();
-        
-        System.out.print("Enter the second number: ");
-        int y = mul.nextInt();
-        
-        int multiplication = x * y;
-        System.out.println("The total is " + multiplication);
-    }
+public static int mul(int firstNum, int secondNum){
+    int result = firstNum * secondNum;
+    return result;
 }
