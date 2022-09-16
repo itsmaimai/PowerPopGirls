12a84616d86cd3671dcb7e6563d6c5ae77c6d52e
itsmaimai <21103807@usc.edu.ph>
2022-09-16 20:18:52 +0800

    Squashed commit of the following:
    commit bfb302939128c58ad49733e05d008cf223b717b7
    Author: ethyl28 <112525491+ethyl28@users.noreply.github.com>
    Date:   Fri Sep 16 20:09:51 2022 +0800
    
        initial commit

--- Calcu.java
+++ Calcu.java
@@ -2,21 +2,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import java.util.Scanner;
 
-/**
- *
- * @author Student.admin
- */
-public class display {
-    public static void main(String[] args){
-        addition add = new addition();
-        subtraction sub = new subtraction();
-        multiplication mul = new multiplication();
-        division div = new division();
-        add.result();
-        sub.result();
-        mul.result();
-        div.result();
 
+public class Calcu{
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int firstNum,secondNum;
+        System.out.println("Enter the first number: ");
+        firstNum = scan.nextInt();
+      
+        System.out.println("Enter second number: ");
+        secondNum = scan.nextInt();
+
+        System.out.println("Enter your choice: 1. Addition, 2. substraction 3. Multiplication and 4. Division:");
+        int choose;
+        choose = scan.nextInt();
+        scan.close();
+        switch (choose){
+        case 1:
+            System.out.println("The result is: "+ add(firstNum,secondNum);
+            break;
+            
+        case 2:
+            System.out.println("The result is: "+ sub(firstNum,secondNum);
+            break;      
+        case 3:
+            System.out.println("The result is: "+ mult(firstNum,secondNum);
+            break;
+        case 4:
+            System.out.println("The result is: "+ div(firstNum,secondNum);
+            break;
+            default:
+                System.out.println("Error, Please see the choices provided");
+        }
     }
 }
