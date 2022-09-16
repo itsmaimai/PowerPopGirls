3884970af64f702431e248905d6a88d85cc34c66
ethyl28 <delossantos.sotero.ethyl@gmail.com>
2022-09-16 22:13:43 +0800

    Revert "Delete Calcu.java"
    This reverts commit f6fb817d4f5a2c5856a71f7a44abecbe15d9a820.

---
+++ Calcu.java
@@ -0,0 +1,60 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+
+public class Calcu {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int firstNum, secondNum;
+        System.out.println("Enter the first number: ");
+        firstNum = scan.nextInt();
+
+        System.out.println("Enter second number: ");
+        secondNum = scan.nextInt();
+
+        System.out.println("Enter your choice: 1. Addition, 2. substraction 3. Multiplication and 4. Division:");
+        int choose;
+        choose = scan.nextInt();
+        scan.close();
+        switch (choose) {
+            case 1:
+                System.out.println("The result is: " + add(firstNum, secondNum));
+                break;
+
+            case 2:
+                System.out.println("The result is: " + sub(firstNum, secondNum));
+                break;
+            case 3:
+                System.out.println("The result is: " + mul(firstNum, secondNum));
+                break;
+            case 4:
+                System.out.println("The result is: " + div(firstNum, secondNum));
+                break;
+            default:
+                System.out.println("Error, Please see the choices provided");
+        }
+    }
+
+    public static int sub(int firstNum, int secondNum) {
+        int result = firstNum - secondNum;
+        return result;
+    }
+
+    public static int add(int firstNum, int secondNum) {
+        int result = firstNum + secondNum;
+        return result;
+    }
+
+    public static int mul(int firstNum, int secondNum) {
+        int result = firstNum * secondNum;
+        return result;
+    }
+
+    public static int div(int firstNum, int secondNum) {
+        int result = firstNum / secondNum;
+        return result;
+    }
+}
